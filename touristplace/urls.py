7d7cfc6a7eb83dd3c348b0c3301e7f7d52412ff0
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-24 12:57:05 +0200

    Add home.html to templates folder

--- touristplace/urls.py
+++ touristplace/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('account/settings/', views.account_settings, name='account_settings'),
     path('state_detail/<int:state_id>/', views.state_detail, name='state_detail'),
 ]
