5438c23b6e5d1ac485681a987caf19f832825fc3
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-24 21:27:41 +0200

    Register package and booking model in admin.py

--- package/admin.py
+++ package/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Package,Booking
 
-# Register your models here.
+admin.site.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('state', 'package_type', 'price', 'places_limit')
+    list_filter = ('state', 'package_type')
+    search_fields = ('state__name',)
+
+admin.site.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date', 'no_of_guests', 'package', 'status', 'payment_amount']
+    list_filter = ['user', 'status']
+    search_fields = ['user__username', 'package__state__name']
