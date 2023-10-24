5c17bdc599143d0d1b5c571a38b20b0c2b507f9e
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-24 16:04:58 +0200

    feat: Implement place_details view

--- touristplace/views.py
+++ touristplace/views.py
@@ -32,3 +32,16 @@ def state_detail(request, state_id):
     }
 
     return render(request, 'state_detail.html', context)
+
+
+@login_required
+def place_details(request, place_id):
+    place = get_object_or_404(TouristPlace, pk=place_id)
+    tourist_place = TouristPlace.objects.get(pk=place_id)
+    default_image = TouristPlaceImage.objects.filter(touristplace=place, default_image=True).first()
+    place_image_url = default_image.image_url if default_image else None
+
+    # Retrieve reviews associated with the tourist place
+    reviews = Review.objects.filter(tourist_place=place)
+
+    return render(request, 'place_detail.html', {'place': place, 'place_image_url': place_image_url, 'reviews': reviews})
