a0b39db3393eb35be0f590e7070e5e64c32e66fd
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-24 21:34:57 +0200

    Implemented Package List View

--- package/views.py
+++ package/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class PackageListView(ListView):
+    model = State
+    template_name = 'pack_list.html'
+    context_object_name = 'states'
