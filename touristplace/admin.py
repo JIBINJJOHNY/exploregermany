dbc05e1d3aa1ecc23ebd5572516973093100edba
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-24 09:57:21 +0200

    Register Touristplace model and Touristplaceimage model

--- touristplace/admin.py
+++ touristplace/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import State
+from .models import State,TouristPlace
 
 admin.site.register(State)
 class StateAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
+
+admin.site.register(TouristPlace, TouristPlaceAdmin)
+class TouristPlaceAdmin(admin.ModelAdmin):
+    inlines = [TouristPlaceImageInline]
+    list_display = ('name', 'location', 'state')
+    list_filter = ('state',)
+    search_fields = ('name', 'location', 'state')
+
+class TouristPlaceImageInline(admin.TabularInline):
+    model = TouristPlaceImage
+    extra = 1  # Number of empty image forms to display
