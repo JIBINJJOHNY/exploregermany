3cf6976d58e15256968798fb5eb84cd19d368dfd
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-24 11:41:31 +0200

    Add urls.py to touristplace app

--- touristplace/views.py
+++ touristplace/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from .models import State
+from django.contrib.auth.decorators import login_required
+
+
 def home(request):
     states = State.objects.all()
     context = {
         'states': states,
     }
     return render(request, 'home.html', context)
+@login_required
+def state_detail(request, state_id):
+    state = get_object_or_404(State, pk=state_id)
+    tourist_places = TouristPlace.objects.filter(state=state)
+
+    # Calculate average ratings for each tourist place
+    for place in tourist_places:
+        place.average_rating = Rev.objects.filter(tourist_place=place).aggregate(avg_rating=Avg('rating'))['avg_rating']
+
+    context = {
+        'state': state,
+        'tourist_places': tourist_places,
+    }
+
+    return render(request, 'state_detail.html', context)
