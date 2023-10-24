d02f384f5f7e515ee5133b847f539262c8a485e6
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-24 16:06:17 +0200

    Add URL path for place_details view

--- touristplace/urls.py
+++ touristplace/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('account/settings/', views.account_settings, name='account_settings'),
     path('state_detail/<int:state_id>/', views.state_detail, name='state_detail'),
+    path('place/<int:place_id>/', views.place_details, name='place_details'),
 ]
