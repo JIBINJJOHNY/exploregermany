46c7f7213cc56645ae88e925b378f5779c7b5d17
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-25 14:58:51 +0200

    Update allowedhost for deployment

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['exploregermany-b189583e9e95.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['exploregermany-c3b93e179fbb.herokuapp.com','127.0.0.1']
 
 
 # Application definition
