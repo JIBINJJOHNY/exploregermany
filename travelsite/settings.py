28f97e5ef7387593e0387a6903860e8a1c124c17
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-26 09:51:27 +0200

    Remove django heroku

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-import django_heroku
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -176,4 +175,3 @@ EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_ID')
 EMAIL_HOST_PASSWORD = os.environ.get('APP_PASSWORD')
 EMAIL_DEBUG = True
-django_heroku.settings(locals())
