d6631bcb368cd24db8d5fddbf643dfdbb561fbbc
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-10-26 10:32:45 +0200

    Update static settings for deployment

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -140,7 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
